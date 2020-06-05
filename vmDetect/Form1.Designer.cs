6abc79a71a409545f7a86a612561e033cfaeaf9c
NQNT <54726985+nqntmqmqmb@users.noreply.github.com>
2020-06-06 01:03:11 +0200

    Add files via upload

---
+++ vmDetect/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace vmDetect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(96, 139);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(88, 32);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Build !";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(96, 61);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(88, 31);
+            this.Button2.TabIndex = 2;
+            this.Button2.Text = "Browse";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 183);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "vmDetect";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Button2;
+    }
+}
+
---
+++ vmDetect/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vmDetect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            File.WriteAllBytes(Directory.GetCurrentDirectory() + "\\" + "Stub.exe", Properties.Resources.Stub);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName + "|" + openFileDialog1.FileName.Split('.')[1];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] ext = textBox1.Text.Split('|'); // file extension in [1]
+            Byte[] bytes = File.ReadAllBytes(ext[0].Replace("\\", "\\\\"));
+            String file = Convert.ToBase64String(bytes);
+            using(FileStream filestream = new FileStream("Stub.exe", FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+            {
+                using(BinaryWriter binaryWriter = new BinaryWriter(filestream))
+                {
+                    filestream.Position = filestream.Length + 1;
+                    binaryWriter.Write("***" + file + "|" + ext[1]);
+                    MessageBox.Show("Anti-VM added to the file");
+                }
+            }
+        }
+    }
+}
